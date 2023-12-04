f8ad5bfc4203c2e05d26141da7847a756947f052
Md.Fazlul Haque <tuhinbadhon@gmail.com>
2023-12-04 12:58:20 +0600

    first commit

---
+++ src/components/pages/Services.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const Services = () => {
+  return (
+    <div>
+      <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime alias
+        ut doloremque rem impedit, voluptate odit sapiente nemo facere dolores
+        hic rerum quo earum aspernatur officiis cupiditate culpa nostrum
+        recusandae quod laudantium obcaecati. Consectetur doloremque, quia
+        dolorum alias accusamus tempore nobis quaerat. Fugiat maxime commodi
+        dolor suscipit tenetur ea dicta repellat atque. Amet ea enim optio quos.
+        A distinctio facilis dicta numquam expedita non, quod reprehenderit
+        neque nam? Reprehenderit, sapiente pariatur aut dolore sunt consectetur
+        assumenda expedita quibusdam recusandae repellat voluptas accusamus vel
+        natus voluptatem, illum similique aperiam architecto? Tenetur, officia
+        ea? Ex cum in eos harum dolores natus tempora?
+      </div>
+    </div>
+  );
+};
